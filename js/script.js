8eb8831ee0dd1de9f2a81c0c097ab8bd2c2398d2
MagicMarka <magicformarka@gmail.com>
2017-05-27 13:17:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+   $(document).ready(function(){
+
+      $('.team-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+
+});
+   $('.testimonials-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: false
+});
+
+    });
